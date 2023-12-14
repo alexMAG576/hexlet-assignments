a27ac1c3fa92400163299f6fa50b9efbd9a5d1ac
@hexlet/cli <support@hexlet.io>
2023-12-15 01:10:24 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -1,16 +1,17 @@
 package exercise;
 
 import java.util.List;
-import java.util.stream;
+import java.util.stream.Comparator;
 
 // BEGIN
 public class App {
-    public static List<String> buildApartmentsList(List<Home> apartments, int count) {
-        return apartments.stream()
-                .sorted(Home::compareTo)
-                .limit(count)
-                .map(Home::toString)
-                .toList();
+    public static List<String> buildApartmentsList2(List<Home> apartments, int count) {
+        int normalizedCount = Math.min(count, apartments.size());
+        apartments.sort(Home::compareTo);
+        List<Home> sublist = apartments.subList(0, normalizedCount);
+        return sublist.stream()
+                .map(appartment -> appartment.toString())
+                .collect(Collectors.toList());
     }
 }
 // END
