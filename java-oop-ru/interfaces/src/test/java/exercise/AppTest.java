18d83bddfdaf6c57875490db8e5eda17bdab8461
@hexlet/cli <support@hexlet.io>
2023-12-15 09:09:34 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
+++ java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
@@ -61,7 +61,7 @@ class AppTest {
     void testReversedSequence() {
         CharSequence text = new ReversedSequence("abcdef");
         assertThat(text.toString()).isEqualTo("fedcba");
-        assertThat(text.charAt(1)).isEqualTo("e");
+        assertThat(text.charAt(1)).isEqualTo('e');
         assertThat(text.length()).isEqualTo(6);
         assertThat(text.subSequence(1, 4).toString()).isEqualTo("edc");
     }
