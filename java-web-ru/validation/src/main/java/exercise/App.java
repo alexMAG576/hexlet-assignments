1b1f4b867d27e966787a11ec9b1fbb8cfa787a7b
@hexlet/cli <support@hexlet.io>
2024-02-28 00:59:36 +0300

    submit java-web-ru/validation

--- java-web-ru/validation/src/main/java/exercise/App.java
+++ java-web-ru/validation/src/main/java/exercise/App.java
@@ -35,25 +35,25 @@ public final class App {
         });
 
         app.post("/articles", ctx -> {
-
             try {
                 var title = ctx.formParamAsClass("title", String.class)
                         .check(value -> value.length() >= 2, "Название не должно быть короче двух символов")
                         .check(value -> !ArticleRepository.existsByTitle(value), "Статья с таким названием уже существует")
                         .get();
+
                 var content = ctx.formParamAsClass("content", String.class)
                         .check(value -> value.length() >= 10, "Статья должна быть не короче 10 символов")
                         .get();
 
-
                 var article = new Article(title, content);
                 ArticleRepository.save(article);
                 ctx.redirect("/articles");
+
             } catch (ValidationException e) {
                 var title = ctx.formParam("title");
                 var content = ctx.formParam("content");
                 var page = new BuildArticlePage(title, content, e.getErrors());
-                ctx.render("articles/build.jte", Collections.singletonMap("page", page));
+                ctx.render("articles/build.jte", Collections.singletonMap("page", page)).status(422);
             }
         });
         // END
