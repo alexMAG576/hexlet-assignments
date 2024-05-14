c00f69e8fa5813560d25c262e23dfffdf459faff
@hexlet/cli <support@hexlet.io>
2024-05-14 15:45:50 +0300

    submit java-spring-ru/error-handling

--- java-spring-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
@@ -37,13 +37,25 @@ public class ProductsController {
     }
 
     // BEGIN
-    @GetMapping("/products/{id}")
-    @PutMapping("/products/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public User show(@PathVariable Long id) {
-        var user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(id + " Not Found"));
-        return user;
+    @GetMapping(path = "/{id}")
+    public Product show(@PathVariable long id) {
+        var product =  productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        return product;
+    }
+
+    @PutMapping(path = "/{id}")
+    public Product update(@PathVariable long id, @RequestBody Product productData) {
+        var product =  productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        product.setTitle(productData.getTitle());
+        product.setPrice(productData.getPrice());
+
+        productRepository.save(product);
+
+        return product;
     }
     // END
 
